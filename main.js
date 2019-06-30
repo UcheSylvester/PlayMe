fc298f730e89a252c6e8f3976c01a655974189c2
Uchenna <okorocode@gmail.com>
2019-06-30 23:19:28 +0100

    implement cancel and dragOver

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('working')
+console.log('working');
+
+const dropTarget = document.getElementById('dropTarget');
+console.log(dropTarget)
+
+function cancel(e) {
+    if(e.preventDefault) e.preventDefault()
+    if(e.stopPropagation) e.stopPropagation()
+    console.log('canceling default behavior...')
+    return false;
+};
+
+function dragOver(e) {
+    cancel(e);
+    // copying files from file system
+    console.log('copying files from file system...')
+    e.dataTransfer.dropEffect = 'copy';
+}
+
+dropTarget.addEventListener('dragenter', cancel)
+dropTarget.addEventListener('dragover', dragOver)
+dropTarget.addEventListener('dragleave', cancel)
+dropTarget.addEventListener('drop', dropped)
