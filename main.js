022177c8e7c3c77253b1ea02ee763591b5c0992b
Uchenna <okorocode@gmail.com>
2019-07-05 20:48:59 +0100

    faced a blocker: find relative path

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const playground = document.getElementById('playground');
 function cancel(e) {
     if(e.preventDefault) e.preventDefault();
     if(e.stopPropagation) e.stopPropagation()
-    console.log('cancelling default behavior...')
+    // console.log('cancelling default behavior...')
     return false;
 }
 
@@ -15,7 +15,30 @@ function cancel(e) {
 function dragOver(e) {
     cancel(e);
     e.dataTransfer.dropEffect = 'copy';
-    console.log('copying files from file system')
+    // console.log('copying files from file system')
+}
+
+function dropped(e) {
+    cancel(e);
+    console.log('dropped...')
+
+    let target = this;
+    let video, music;
+    const types = e.dataTransfer.types;
+    // console.log(types)
+    types.forEach(type => {
+        if(type === 'Files') {
+            const files = e.dataTransfer.files;
+            console.log(files)
+
+            /**
+            BLOCKER:
+            Find the path to the files(video/ audio) dropped
+            this will serve as the src attribute
+            **/
+        }
+    })
+
 }
 
 playground.addEventListener('dragenter', cancel);
