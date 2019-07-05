3b665cac1a12f9902881e7ca3cde37d9e9b477e2
Uchenna <okorocode@gmail.com>
2019-07-05 18:19:53 +0100

    working on drag and drop!

--- main.js
+++ main.js
@@ -1,34 +1,10 @@
 console.log('working');
 
-// const dropTarget = document.getElementById('dropTarget');
-// console.log(dropTarget)
-
-// function cancel(e) {
-//     if(e.preventDefault) e.preventDefault()
-//     if(e.stopPropagation) e.stopPropagation()
-//     console.log('canceling default behavior...')
-//     return false;
-// };
-
-// function dragOver(e) {
-//     cancel(e);
-//     // copying files from file system
-//     console.log('copying files from file system...')
-//     e.dataTransfer.dropEffect = 'copy';
-// }
-
-// function dropped(e) {
-//     cancel(e)
-// }
-
-// dropTarget.addEventListener('dragenter', cancel)
-// dropTarget.addEventListener('dragover', dragOver)
-// dropTarget.addEventListener('dragleave', cancel)
-// dropTarget.addEventListener('drop', dropped)
 
 const playlist = document.getElementById('playlist');
 const playground = document.getElementById('playground');
 
+// canceling default behavior
 function cancel(e) {
     if(e.preventDefault) e.preventDefault();
     if(e.stopPropagation) e.stopPropagation()
@@ -36,6 +12,7 @@ function cancel(e) {
     return false;
 }
 
+// copying file on drag over
 function dragOver(e) {
     cancel(e);
     e.dataTransfer.dropEffect = 'copy';
